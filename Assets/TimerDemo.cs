c756d516c5f6a827d47e3277a5e96eeabb65dedf
Prudhvi Raj Patnala <pruthvipatnala@gmail.com>
2022-09-11 18:13:15 -0700

    feat(popUp and SceneSwitch): added gameover popup and sceneswitch features

--- Assets/TimerDemo.cs
+++ Assets/TimerDemo.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class TimerDemo : MonoBehaviour
 {
     [SerializeField] Timer timer1 ;
+    public GameObject GameOverMenu;
+
     // Start is called before the first frame update
     void Start()
     {
+        GameOverMenu.SetActive(false);
         timer1
-        .SetDuration(10)
-        .OnEnd(() => Debug.Log ("Timer 1 ended"))
+        .SetDuration(5)
+        .OnEnd(() => {
+            Debug.Log("Timer 1 ended");
+            GameOverMenu.SetActive(true);
+        })
         .Begin();
-        // TODO: Gameover popup OnEnd
     }
 
     // Update is called once per frame
