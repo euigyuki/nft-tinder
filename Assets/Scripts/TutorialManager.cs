ea7010683d6b8716dd002b7c680fdd7b0d441aaa
yechan96 <yechanlee96@gmail.com>
2022-09-20 13:13:12 -0700

    tutorial done

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] tutorialCards = new GameObject[4];
+    private int index=0; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i =1; i<tutorialCards.Length;i++){
+            tutorialCards[i].SetActive(false);
+        }
+        tutorialCards[0].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("right")||Input.GetKeyDown("left")){
+            nextCard();
+        }   
+    }
+
+    public void nextCard(){
+        tutorialCards[index++%tutorialCards.Length].SetActive(false);
+        tutorialCards[index%tutorialCards.Length].SetActive(true);
+    }
+
+    public void startGame(){
+        SceneManager.LoadScene("hype");
+    }
+}
