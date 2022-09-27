f3e70dd5abb005ccb6070a1547a96be26f0cb602
yechan96 <yechanlee96@gmail.com>
2022-09-27 12:10:38 -0700

    card fade effect

--- Assets/Scripts/nftGenerator.cs
+++ Assets/Scripts/nftGenerator.cs
@@ -10,6 +10,7 @@ public class nftGenerator : MonoBehaviour
 
     public GameObject rotPivot;
     public float duration;
+    public int rotAngle;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +26,14 @@ public class nftGenerator : MonoBehaviour
         }
 
         if(Input.GetKeyDown("t")){
-            StartCoroutine(rotateAnimation(1));
+            // StartCoroutine(rotateAnimation(1));
+            StartCoroutine(swipeEffect(false));
+            Debug.Log(Screen.width);
         }
 
         if(Input.GetKeyDown("r")){
-            StartCoroutine(rotateAnimation(-1));
+            // StartCoroutine(rotateAnimation(-1));
+            StartCoroutine(swipeEffect(true));
         }
     }
 
@@ -52,6 +56,13 @@ public class nftGenerator : MonoBehaviour
         }
     }
 
+    public void setColor(Color newColor){
+        for(int i = 0; i<imageParts.Length; i++){
+            imageHolder temp = imageParts[i];
+            temp.imagePart.color = newColor;
+        }
+    }
+
     IEnumerator rotateAnimation(int direction){
         Vector3 axis = new Vector3(0,0,1);
         float t = 0.0f;
@@ -64,6 +75,32 @@ public class nftGenerator : MonoBehaviour
             transform.RotateAround(rotPivot.transform.position,axis,direction*rotationAngle*tDelta);
             yield return null;
         }
+    }
+
+    IEnumerator swipeEffect(bool swipeLeft){
+        float time = 0;
+        float fadeStart = duration/1.5f;
+        float rotStart = duration/4f;
+        while (time<duration){
+            time += Time.deltaTime;
+            if(swipeLeft){
+                transform.position = new Vector3(Mathf.SmoothStep(transform.position.x, 
+                transform.position.x-0.025f,time/duration),transform.position.y,transform.position.z);
+                if(time>rotStart)
+                    transform.eulerAngles = new Vector3(0,0,Mathf.LerpAngle(0,rotAngle,(time-rotStart)/(duration-rotStart)));
+            }else{
+                transform.position = new Vector3(Mathf.SmoothStep(transform.position.x, 
+                transform.position.x+0.025f,time/duration),transform.position.y,transform.position.z);
+                if(time>rotStart)
+                    transform.eulerAngles = new Vector3(0,0,Mathf.LerpAngle(0,-1*rotAngle,(time-rotStart)/(duration-rotStart)));
+
+            }
+            if(time>fadeStart){
+                setColor(new Color(1,1,1, Mathf.SmoothStep(1,0,(time-fadeStart)/(duration-fadeStart))));
+            }
+            yield return null;
+        }
+
     }
 }
 
