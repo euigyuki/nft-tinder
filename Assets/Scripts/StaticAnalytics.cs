77eb3a705f3f634590dafee02d69b01974a8b363
Riddhi Patel <riddhipatel2224@gmail.com>
2022-09-27 12:47:15 -0700

    Key press analytics data

--- Assets/Scripts/StaticAnalytics.cs
+++ Assets/Scripts/StaticAnalytics.cs
@@ -6,11 +6,13 @@ public class StaticAnalytics : MonoBehaviour
 {
     // Start is called before the first frame update
 
-     //public static StaticAnalytics instance {get; private set;}
-    public static analyticsData data = new analyticsData();
+    //public static StaticAnalytics instance {get; private set;}
+    //public static analyticsData data = new analyticsData();
     // public static int leftPress = 0;
     // public static int rightPress = 0;
 
+    // private const string projectId = "nft-tinder-b99da"; // You can find this in your Firebase project settings
+    // private static readonly string databaseURL = $"https://{nft-tinder-b99da}.firebaseio.com/";
     void Start()
     {
         
@@ -23,14 +25,14 @@ public class StaticAnalytics : MonoBehaviour
     }
     public static void leftPressIncrement(){
         analyticsData.leftPress += 1;
-        // Debug.Log("leftPressIncrement");
-        // Debug.Log(analyticsData.leftPress);
+        Debug.Log("leftPressIncrement");
+        Debug.Log(analyticsData.leftPress);
     }
 
     public static void rightPressIncrement(){
         analyticsData.rightPress += 1;
-        // Debug.Log("rightPressIncrement");
-        // Debug.Log(analyticsData.rightPress);
+        Debug.Log("rightPressIncrement");
+        Debug.Log(analyticsData.rightPress);
     }
 
     public static string toJson(){
@@ -39,29 +41,35 @@ public class StaticAnalytics : MonoBehaviour
         right=analyticsData.rightPress
         };
         // Debug.Log(data);
-        // analyticsJson ajson = new analyticsJson();
-        // ajson.leftPress=analyticsData.leftPress;
-        // ajson.rightPress=analyticsData.rightPress; 
+        analyticsJson ajson = new analyticsJson();
+        ajson.leftPress=analyticsData.leftPress;
+        ajson.rightPress=analyticsData.rightPress; 
         // Debug.Log(ajson.leftPress);
         // Debug.Log(ajson.rightPress);
         // Debug.Log(analyticsData.leftPress);
         // Debug.Log(analyticsData.rightPress);
-        Debug.Log(data);
-        return JsonUtility.ToJson(data);
+        // Debug.Log(data);
+        return JsonUtility.ToJson(ajson);
     }
+
+    // public static void Post(User user, string userId)
+    // {
+    //     RestClient.Put<analyticsData>(JsonUtility.ToJson(data), user);
+    // }
 
 
 }
 
+// [Serializable()]
 public class analyticsData{
     public static int leftPress = 0;
     public static int rightPress = 0;
 } 
 
-// public class analyticsJson{
-//     public int leftPress = 0;
-//     public int rightPress = 0;
-// } 
+public class analyticsJson{
+    public int leftPress = 0;
+    public int rightPress = 0;
+} 
 
 
 
--- Assets/Scripts/analyticsTest.cs
+++ Assets/Scripts/analyticsTest.cs
@@ -20,8 +20,4 @@ public class analyticsTest : MonoBehaviour
         WebGLFileSaver.SaveFile(StaticAnalytics.toJson(),"dataJson.json");
 
     }
-
-    public void testing(){
-        Debug.Log("testing");
-    }
 }
