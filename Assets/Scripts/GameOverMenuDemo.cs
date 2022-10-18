4c096e92b35121988da56bb443c1df14e61fa0a6
Surili19 <surilihansalia19@gmail.com>
2022-10-17 19:27:34 -0700

    GameEndUpdate
    Update on displaying text

--- Assets/Scripts/GameOverMenuDemo.cs
+++ Assets/Scripts/GameOverMenuDemo.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System;
 
 public class GameOverMenuDemo : MonoBehaviour
 {
+    public Text textMesh;
+    public double profit;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +20,7 @@ public class GameOverMenuDemo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        setScoreText();
         
     }
 
@@ -34,4 +40,21 @@ public class GameOverMenuDemo : MonoBehaviour
         Debug.Log("Quit Button Clicked");
         Application.Quit();
     }
+     void setScoreText() {
+        Debug.Log("Money"+PriceManager.walletValue);
+        profit = PriceManager.walletValue-5000;
+        if(profit>0)
+        {
+            Debug.Log("Positive profit" + profit);
+            textMesh.text="Congratulations you have made an overall profit of " + string.Format("{0:0.##}", profit + " $");
+        }
+        else
+        {
+            Debug.Log("Negative loss" + Math.Abs(profit));
+            textMesh.text="So Sorry you have encurred an overall loss of " + string.Format("{0:0.##}",  Math.Abs(profit) + " $");
+
+        }
+        // textMesh.text = string.Format("{0:0.##}", PriceManager.walletValue);
+    }
+
 }
