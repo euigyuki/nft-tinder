39f714fb58a95e2ef8b21d0d74681b2096279702
yechan96 <yechanlee96@gmail.com>
2022-11-22 19:26:03 -0800

    leadeboard score prefab script

---
+++ Assets/Scripts/LeaderboardScores.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LeaderboardScores : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
